88a985c0e7575675a678a22c11f342ceeaaa7c3a
Justin Wong <4376095+jus888@users.noreply.github.com>
2024-04-10 18:21:55 +0200

    Basic JSON parsing with file input

---
+++ scripts/search.js
@@ -0,0 +1,26 @@
+const jsonInput = document.querySelector("#json-file");
+const resultContainer = document.querySelector(".result-container");
+
+function createResultElement(resultData) {
+    const resultElement = document.createElement("div");
+    resultElement.classList.add("result");
+    resultElement.textContent = resultData["dc_title"];
+    return resultElement;
+}
+
+function populateResults(hitData) {
+    const resultElements = hitData.map(createResultElement);
+    resultContainer.innerHTML = "";
+    resultElements.forEach(element => {
+        resultContainer.appendChild(element);
+    });
+}
+
+jsonInput.addEventListener("change", () => {
+    const file = jsonInput.files[0];
+    const resultsText = file.text();
+    resultsText.then(text => {
+        const results = JSON.parse(text);
+        populateResults(results["hits"]);
+    });
+});
