7d6f00e8c6329b7badfbb790b62921874cbc770e
jus888 <4376095+jus888@users.noreply.github.com>
2024-04-12 21:18:11 +0200

    More reasonable layout and code
    Displays the results vertically, like Woogle itself. The search bar is positioned left of the results. Flexbox is applied to make the website responsive to some degree.
    
    Uses html <template> to simplify javascript.

--- scripts/search.js
+++ scripts/search.js
@@ -1,28 +1,30 @@
-function createResultElement(resultData) {
-    const resultElement = document.createElement("div");
-    resultElement.classList.add("result");
-    resultElement.innerHTML = resultData["dc_title"];
-    return resultElement;
+function createResultElement(hitData, parent) {
+    const resultElement = resultTemplate.content.cloneNode(true);
+    const title = resultElement.querySelector("h1");
+    const para = resultElement.querySelector("p");
+    title.innerHTML = hitData["dc_title"];
+    para.innerHTML = hitData["dc_description"] ?? "Geen omschrijving";
+
+    parent.appendChild(resultElement);
 }
 
 function populateResults(hitData) {
-    const resultElements = hitData.map(createResultElement);
-    resultContainer.innerHTML = "";
-    resultElements.forEach(element => {
-        resultContainer.appendChild(element);
-    });
+    resultContainer.innerHTML = "";    
+    hitData.forEach(hit => createResultElement(hit, resultContainer))
 }
 
 const searchForm = document.querySelector("form");
 const searchSubmit = document.querySelector("form > button");
-const resultContainer = document.querySelector(".result-container");
+
+const resultTemplate = document.querySelector("template");
+const resultContainer = document.querySelector(".search-results");
 
 searchSubmit.addEventListener("click", async function () {
     const formData = new FormData(searchForm);
     const queryString = new URLSearchParams(formData).toString();
     const file = await fetch("https://woogle.wooverheid.nl/search?" + queryString, {
         method: "get"
-    })
+    });
 
     const resultsText = await file.text();
     const results = JSON.parse(resultsText);
