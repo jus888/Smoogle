ec60f51cf0904f216718282cbfd5d449b961e1ed
jus888 <4376095+jus888@users.noreply.github.com>
2024-04-13 17:59:03 +0200

    Filter results with specified supplier

--- scripts/search.js
+++ scripts/search.js
@@ -16,8 +16,31 @@ function populateResults(hitData) {
     hitData.forEach(hit => createResultElement(hit, resultContainer))
 }
 
+const dataLists = [
+    "publisher"
+]
+
+function replaceListValuesFormData(formData) {
+    for (const key of dataLists) {
+        const value = formData.get(key);
+        if (!value) {
+            continue;
+        }
+
+        const optionElement = document.querySelector(`option[value="${value}"]`);
+        if (!optionElement) {
+            formData.delete(key);
+            continue;
+        }
+
+        formData.set(key, optionElement.getAttribute("data-value"));
+    }
+}
+
 async function search() {
     const formData = new FormData(searchForm);
+    replaceListValuesFormData(formData);
+
     const queryString = new URLSearchParams(formData).toString();
     const file = await fetch("https://woogle.wooverheid.nl/search?" + queryString, {
         method: "get"
@@ -29,7 +52,7 @@ async function search() {
 }
 
 const searchForm = document.querySelector("form");
-const searchSubmit = document.querySelector("form > button");
+const searchSubmit = document.querySelector(".search-bar > button");
 
 const resultTemplate = document.querySelector("template");
 const resultContainer = document.querySelector(".search-results");
